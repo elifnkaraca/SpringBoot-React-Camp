1fe7ea86898ffb0221a7549569c3f40453a6f033
elifnkaraca <elifnkaraca@users.noreply.github.com>
2021-06-06 19:49:19 +0300

    services added

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -1,6 +1,8 @@
+  
 package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
+
 
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
@@ -9,6 +11,27 @@ import kodlamaio.northwind.entities.concretes.Product;
 public interface ProductService {
 	
 	DataResult<List<Product>> getAll();
+	
+	DataResult<List<Product>> getAll(int pageNo, int pageSize);
+	
+	DataResult<List<Product>> getAllSorted();
+	
 	Result add(Product product);
+	
+	DataResult<Product> getByProductName(String productName);
+	
+	DataResult<Product> getByProductNameAndCategory(String productName, int categoryId);
+
+	DataResult<List<Product>> getByProductNameOrCategory(String productName, int categoryId);
+
+	DataResult<List<Product>> getByCategoryIn(List<Integer> categories);
+	
+	DataResult<List<Product>> getByProductNameContains(String productName);
+	
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+	
+	DataResult<List<Product>> getByNameAndCategory(String productName, int categoryId);
+
+	
 
 }
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,13 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @Entity
 @Table(name = "products")
 @AllArgsConstructor
@@ -22,9 +22,6 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "product_id")
 	private int id;
-	
-	@Column(name = "category_id")
-	private int categoryId;
 	
 	@Column(name = "product_name")
 	private String productName;
@@ -37,5 +34,57 @@ public class Product {
 	
 	@Column(name = "quantity_per_unit")
 	private String quantityInStock;
+	
+	@ManyToOne()
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public short getUnitsInStock() {
+		return unitsInStock;
+	}
+
+	public void setUnitsInStock(short unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
+
+	public String getQuantityInStock() {
+		return quantityInStock;
+	}
+
+	public void setQuantityInStock(String quantityInStock) {
+		this.quantityInStock = quantityInStock;
+	}
+	
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
 	
 }
